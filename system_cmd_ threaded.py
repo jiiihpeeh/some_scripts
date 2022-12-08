fa8a88c284aad8863ca4e480e21d4858fcc5a670
jiiihpeeh <68808867+jiiihpeeh@users.noreply.github.com>
2022-12-08 16:51:22 +0200

    Create system_cmd_ threaded.py

---
+++ system_cmd_ threaded.py
@@ -0,0 +1,21 @@
+#Run system command executing list of items in threaded mode 
+
+from subprocess import run
+from glob import glob
+from threading import Thread
+from threading import active_count
+from os import cpu_count
+from time import sleep
+
+file_list = glob('*.png')
+threads=cpu_count()
+
+def optipng(png):
+    run(['optipng', '-O8', png])
+
+for i in file_list:
+    t = Thread(target=optipng, args=[i])
+    while active_count() >= threads:
+        sleep(1e-5)
+    t.start()
+t.join()
